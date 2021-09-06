2038ac069b467202311753cd1d03f8e4d30088b2
TheNinza <niks.a3198@gmail.com>
2021-09-06 18:11:31 +0530

    fixed theme and created spacer component for flexibility

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { theme } from "./src/infrastructure/theme";
+import { theme } from "./src/infrastructure/theme/index.js";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -10,15 +10,18 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
-  useOswald({ Oswald_400Regular });
-  useLato({ Lato_400Regular });
+  const [isOswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [isLatoLoaded] = useLato({ Lato_400Regular });
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <RestaurantScreen />
-        <ExpoStatusBar style="light" />
-      </ThemeProvider>
-    </>
+    isOswaldLoaded &&
+    isLatoLoaded && (
+      <>
+        <ThemeProvider theme={theme}>
+          <RestaurantScreen />
+          <ExpoStatusBar style="light" />
+        </ThemeProvider>
+      </>
+    )
   );
 }
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { Text, Image } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 import styled from "styled-components/native";
 import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -33,17 +36,27 @@ const Rating = styled.View`
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://images.pexels.com/photos/3887985/pexels-photo-3887985.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     ],
     address = "100 some random street address",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = false,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -58,11 +71,26 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, idx) => (
+              <SvgXml key={idx} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant="left.large" />
+            {isOpenNow && !isClosedTemporarily && (
+              <SvgXml xml={open} width={20} height={20} />
+            )}
+            <Spacer variant="left.large" />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
